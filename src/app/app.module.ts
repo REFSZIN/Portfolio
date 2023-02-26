874180b7f1cbac24a31e50d3d5a4fdbe2c59de09
Yan Matheus <95008410+REFSZIN@users.noreply.github.com>
2023-02-25 23:56:44 -0300

    Build: 😍 Component Carrosel Implemented

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import {MatIconModule} from '@angular/material/icon';
+import {MatGridListModule} from '@angular/material/grid-list';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule} from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
+import { CarouselModule } from '@coreui/angular';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    CarouselModule,
+    MatMenuModule,
+    MatButtonModule,
+    MatCardModule,
+    MatToolbarModule,
+    MatGridListModule,
     MatIconModule,
     MatSlideToggleModule,
     BrowserModule,
